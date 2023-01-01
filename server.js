fa51ea010eab87864d6b8f5fd55e3ca3bbcf044f
Ashiqul Islam <i_simanto_i@live.com>
2023-01-02 05:55:00 +0600

    express

--- server.js
+++ server.js
@@ -108,8 +108,8 @@ app.use("/api/v1/admin", verifyAdmin, statusRoute);
 app.use("/api/v1/admin", verifyAdmin, invoiceRoute);
 
 
-app.get("*", (req,res)=>{
-    res.sendFile(path.resolve(__dirname, './merchant/build', 'index.html'))
+app.get("/*", (req,res)=>{
+    res.sendFile(path.join(__dirname, './merchant/build', 'index.html'))
 })
 
 app.use((err,req,res,next) => {
