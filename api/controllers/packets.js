e267e5954078d064c47ab37dcc44719a574829bb
Ashiqul Islam <i_simanto_i@live.com>
2023-08-05 20:45:23 +0600

    api - asssigned packets to agent

--- api/controllers/packets.js
+++ api/controllers/packets.js
@@ -1210,86 +1210,81 @@ export const packetReturned = async (req,res,next)=>{
 export const packetAssignedforDeliveries = async(req,res,next)=>{
   try {
     // console.log("agent id:",req.user.id)
-    const packets = await User.aggregate([
-      {$match:{_id: new mongoose.Types.ObjectId(req.user.id)}},
+    // const packets = await User.aggregate([
+    //   {$match:{_id: new mongoose.Types.ObjectId(req.user.id)}},
+    //   {
+    //     $lookup:{
+    //       from: "packets",
+    //       let:{id: req.user.id},
+    //       pipeline:[
+    //         {$match:{$expr:{$eq:["$delivery_man", "$$id"]}}},
+    //         {$project:{
+    //           "_id": 0,
+    //           packetID: "$_id",
+    //           packet_trackingID: "$trackingID",
+    //           packet_createdAt: "$createdAt",
+    //           packet_customerName:{ "$arrayElemAt": ["$customer.name", 0] },
+    //           packet_customerPhone:{ "$arrayElemAt": ["$customer.phone", 0] },
+    //           packet_customerArea:{ "$arrayElemAt": ["$customer.area", 0] },
+    //           packet_customerAddress:{ "$arrayElemAt": ["$customer.address", 0] },
+    //           packet_status:{ "$arrayElemAt": ["$status.name", -1] },
+    //         }}
+    //       ],
+    //     as: "packets"
+    //   },
+    // },
+    // {
+    //   $project:{
+    //     _id: 1,
+    //     packets:{
+    //       "$filter":{
+    //         "input": "$packets",
+    //         "cond": { 
+    //           "$or": [ 
+    //             {"$eq": ["$$this.packet_status", "assigned-for-delivery"]},
+    //             {"$eq": ["$$this.packet_status", "out-for-delivery"]},
+    //         ]}
+    //       }
+    //     }
+    //   },
+    // },
+    // ]);
+
+
+
+    const packets = await Packet.aggregate([
       {
-        $lookup:{
-          from: "packets",
-          let:{id: req.user.id},
-          pipeline:[
-            {$match:{$expr:{$eq:["$delivery_man", "$$id"]}}},
-            // {
-            //   $lookup:{
-            //     from: "users",
-            //     let:{mid:"$merchantID"},
-            //     pipeline:[
-            //       {$match:{$expr:{$eq:[{$toString:"$_id"}, "$$mid"]}}},
-            //       {
-            //         $lookup:{
-            //           from: "merchant profiles",
-            //           let:{mid:"$merchantProfileID"},
-            //           pipeline:[
-            //             {$match:{$expr:{$eq:[{$toString:"$_id"}, "$$mid"]}}},
-            //           ],
-            //           as: "profile"
-            //         },
-            //       },
-            //     ],
-            //     as: "merchant"
-            //   },
-            // },
-            {
-              $lookup:{
-                from: "customers",
-                let:{cid:"$customerID"},
-                pipeline:[
-                  {$match:{$expr:{$eq:[{$toString:"$_id"}, "$$cid"]}}},
-                ],
-                as: "customer"
-              },
-            },
-            {
-                $lookup:{
-                  from: "status logs",
-                  let:{sid:"$status"},
-                  pipeline:[
-                    {$match:{$expr:{$in:[{$toString:"$_id"}, "$$sid"]}}},
-                  ],
-                  as:"status"
-                }
-            },
-            {$project:{
-              "_id": 0,
-              packetID: "$_id",
-              packet_trackingID: "$trackingID",
-              packet_createdAt: "$createdAt",
-              packet_customerName:{ "$arrayElemAt": ["$customer.name", 0] },
-              packet_customerPhone:{ "$arrayElemAt": ["$customer.phone", 0] },
-              packet_customerArea:{ "$arrayElemAt": ["$customer.area", 0] },
-              packet_customerAddress:{ "$arrayElemAt": ["$customer.address", 0] },
-              packet_status:{ "$arrayElemAt": ["$status.name", -1] },
-            }}
-          ],
-        as: "packets"
-      },
-    },
-    {
-      $project:{
-        _id: 1,
-        packets:{
-          "$filter":{
-            "input": "$packets",
-            "cond": { 
-              "$or": [ 
-                {"$eq": ["$$this.packet_status", "assigned-for-delivery"]},
-                {"$eq": ["$$this.packet_status", "out-for-delivery"]},
+        $match:{
+          "$and":[
+            {delivery_man: req.user.id},
+            {"$or":[
+              {currentStatus:  "assigned-for-delivery"},
+              {currentStatus: "out-for-delivery"},
+              {currentStatus: "done"},
+              {currentStatus: "failed"},
             ]}
-          }
+          ]
         }
       },
-    },
-    ]);
-    res.status(200).json(packets)
+      {
+        $project:{
+          _id: 1,
+          trackingID: "$trackingID",
+          collectionAmount: "$collectionAmount",
+          specialInstruction: "$specialInstruction",
+          currentStatus: "$currentStatus",
+          currentStatusMessage: "$currentStatusMessage",
+          customerName: "$customerName",
+          customerPhone: "$customerPhone",
+          customerArea: "$customerArea",
+          customerAddress: "$customerAddress",
+          merchantName: "$merchantName",
+          merchantPhone: "$merchantPhone",
+        }
+      }
+    ])
+
+    res.status(200).json({"packets": packets, "total": packets.length})
   } catch (err) {
     next(err)
   }
