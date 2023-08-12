49266a0fde8639c9b1a37ae41215609ad4749110
Ashiqul Islam <i_simanto_i@live.com>
2023-08-12 13:24:58 +0600

    updated index file to solve double api call in production

--- merchant/src/index.js
+++ merchant/src/index.js
@@ -7,15 +7,28 @@ import { CookiesProvider } from "react-cookie";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-root.render(
-  <React.StrictMode>
-    <AppProvider >
-      <CookiesProvider>
-        <App />
-      </CookiesProvider>
-    </AppProvider>
-  </React.StrictMode>
-);
+
+if(process.env.NODE_ENV === "DEV"){
+  root.render(
+    <React.StrictMode>
+      <AppProvider >
+        <CookiesProvider>
+          <App />
+        </CookiesProvider>
+      </AppProvider>
+    </React.StrictMode>
+  );
+}else{
+  root.render(
+      <AppProvider >
+        <CookiesProvider>
+          <App />
+        </CookiesProvider>
+      </AppProvider>
+  );
+}
+
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
